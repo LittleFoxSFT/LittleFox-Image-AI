48025c05b9ae4c0be78d636a8e9678af86aed638
Senpai_Desi <contact@little-fox.info>
2023-04-06 08:39:03 +0200

    Added a new image module, Anything Better v3
    Anything better v4
    - old text needs to be updated

--- imgen/imgen.py
+++ imgen/imgen.py
@@ -15,17 +15,25 @@ class imgen(commands.Cog):
     @app_commands.command(name="generate", description="Generate a image with our AI.")
     @app_commands.describe(aimodel = "What AI model you want to use, different models make different images", imagimation = "Your prompt or the text you want to imagine" )
     @app_commands.choices(aimodel = [
-        Choice(name="Stable diffusion", value="stablediff")
+        Choice(name="Stable diffusion", value="stablediff"),
+        Choice(name="Antything Better", value="weeb")
     ])
     async def generate(self, interaction : discord.Interaction, aimodel:str, imagimation:str):
         reader = utils.load_json(assets.jsonfile)
         api = reader["reptoken"]
+        AiGen  = replicate.Client(api_token=api)
         if aimodel == "stablediff":
-            AiGen  = replicate.Client(api_token=api)
             model = AiGen.models.get(name="stability-ai/stable-diffusion")
             version = model.versions.get(id="db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf")
             await interaction.response.send_message("I will send the result in your dms!", ephemeral=True)
             utils.write_log(message=f"{interaction.user.id} -- {interaction.user.name}#{interaction.user.discriminator} has requested a image on {aimodel} with the prompt {imagimation}")
+            output = version.predict(prompt = f"{imagimation}")
+            await interaction.user.send(output[0])
+        elif aimodel == "weeb":
+            model = AiGen.models.get(name="cjwbw/anything-v4.0")
+            version = model.versions.get(id="42a996d39a96aedc57b2e0aa8105dea39c9c89d9d266caf6bb4327a1c191b061")
+            utils.write_log(message=f"{interaction.user.id} -- {interaction.user.name}#{interaction.user.discriminator} has requested a audio file with the prompt {imagimation}")
+            await interaction.response.send_message("Sending the result in your dms. Please allow me some time.", ephemeral=True)
             output = version.predict(prompt = f"{imagimation}")
             await interaction.user.send(output[0])
         else:
